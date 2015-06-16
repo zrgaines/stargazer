14560d2dd77976931714a7f8f737ef2f389d9b49
RobertAKARobin <robertgfthomas@gmail.com>
2015-06-16 16:14:23 -0400

    added sol

--- js/stargazer.js
+++ js/stargazer.js
@@ -1,5 +1,30 @@
 function search(name) {
-  var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
-  // requires "jsonp" datatype.
-  
+  $.ajax({
+      url: 'http://www.strudel.org.uk/lookUP/json/?name='+name,
+      dataType: 'jsonp'
+    })
+    .then(function(data) {
+      var display = '';
+
+      if (data.image) {
+        if (data.image.src.indexOf('//') === 0) {
+          data.image.src = 'http:'+ data.image.src;
+        }
+
+        display = '<img src="'+ data.image.src +'" alt="'+ data.target.name +'">';
+      } else {
+        display = "Whoops, we couldn't find that one!"
+      }
+      
+      $('#preview').html(display);
+    });
 }
+
+
+$('#search').on('submit', function(evt) {
+  evt.preventDefault();
+  
+  var name = escape($('#search-name').val());
+
+  search(name);
+});

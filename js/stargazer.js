9aac29046003d7fa67110616bf4f1a9f4abe2345
Zach Gaines <zrgaines@mtu.edu>
2016-07-11 23:11:24 -0500

    we done here folks

--- js/stargazer.js
+++ js/stargazer.js
@@ -1,5 +1,21 @@
-function search(name) {
-  var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
-  // requires "jsonp" datatype.
-  
-}
+$(document).ready(function() {
+  $('#search').submit(function(e) {
+    e.preventDefault();
+    var searchVal = $('#search-name').val();
+
+    $.ajax({
+      method: "get",
+      url: 'http://www.strudel.org.uk/lookUP/json/',
+      dataType: 'jsonp',
+      data: {
+        name: searchVal
+      }
+    })
+    .done(function(data, textStatus, jqXHR) {
+      if(data.message) {
+        $('#preview').html("<h2>YOU ARE WRONG</h2><br><img src=http://0.media.dorkly.cvcdn.com/77/37/c7358e161859b0a65c09fef0e3996c13.gif>")
+      }
+      else { $('#preview').html("<img src=" + data.image.src + "><img>"); }
+    })
+  })
+});
